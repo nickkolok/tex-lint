101eed077b0765a96ccb27e0b1e3ef0cecde9bcb
NickKolok <nickkolok@mail.ru>
2016-12-03 15:32:03 +0300

    Всякие функции для приведения списка нод к условно-адекватному виду

--- webui/playground.js
+++ webui/playground.js
@@ -21,7 +21,7 @@ function codeLoad(){
 }
 
 function codeSave(){
-	var blob = new Blob([myCodeMirror.getValue().replace(/[\n\r]+/g,"},\r\n{")], {
+	var blob = new Blob([myCodeMirror.getValue().replace(/[\r]*[\n][\r]*/g,"},\r\n{")], {
 		type: "text/plain;charset=utf-8"
 	});
 	var a = document.createElement('a');
@@ -40,16 +40,20 @@ var myCodeMirror = CodeMirror(document.getElementById('code-mirror-holder'));
 myCodeMirror.on("change",codeSave);
 
 
-function getNodesAsIs(){
+function parseToNodes(text){
 	var nodes = [];
 	CodeMirror.runMode(
-		myCodeMirror.getValue(),
+		text,
 		{name:'stex'},
 		function(node,style){
 			nodes.push({text:node,type:style});
 		}
 	);
 	return nodes;
+}
+
+function getNodesAsIs(){
+	return parseToNodes(myCodeMirror.getValue());
 };
 
 function getNodesQuantity(nodes,nodetype,nodetext){
@@ -62,12 +66,51 @@ function getNodesQuantity(nodes,nodetype,nodetext){
 	return quantity;
 }
 
+function isCyryllicText(text){
+	return /^[А-ЯЁ]+$/i.test(text);
+}
+
+
+function markCyrillicNodes(nodes){
+	for(var i = 0; i < nodes.length; i++){
+		if(isCyryllicText(nodes[i].text)){
+			nodes[i].type = "cyrtext";
+		}
+	}
+}
+
+function markSpaceNodes(nodes){
+	for(var i = 0; i < nodes.length; i++){
+		if(nodes[i].text=="\n"){
+			nodes[i].type = "linebreak";
+		} else if(/^\s+$/.test(nodes[i].text)) {
+			nodes[i].type = "space";
+		}
+	}
+}
+
+function joinCyrillicNodes(nodes){
+	for(var i = 0; i < nodes.length; i++){
+		// TODO: объединять не по одной, а по несколько. А то тормозит. Переделать!
+		if(nodes[i].type == "cyrtext" && nodes[i+1].type == "cyrtext"){
+			nodes[i].text += nodes[i + 1].text;
+			nodes.splice(i + 1, 1);
+			i--;
+		}
+	}
+}
+
+function prepareNodes(nodes){
+	markSpaceNodes(nodes);
+	markCyrillicNodes(nodes);
+	joinCyrillicNodes(nodes);
+}
+
 function runcheck(){
 	var nodes = getNodesAsIs();
+	prepareNodes(nodes);
 	console.log(nodes);
 	if(getNodesQuantity(nodes,"tag","\\newcommand")+getNodesQuantity(nodes,"tag","\\renewcommand")){
 		alert('Команды переопределять нельзя!');
 	};
 }
-
-
