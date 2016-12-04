b7215768fba93700186b2e077ae06614b70b8a51
nickkolok <nickkolok@mail.ru>
2016-12-04 15:58:10 +0300

    Nodes выделены в отдельный класс и файл

---
+++ common/Nodes.js
@@ -0,0 +1,71 @@
+// Конструктор
+function Nodes(text) {
+  this.nodes = [];
+  if(text){
+	  this.fromText(text);
+  }
+}
+
+Nodes.prototype.fromText = function(text) {
+	this.nodes = [];
+	var self = this;
+	CodeMirror.runMode(
+		text,
+		{name:'stex'},
+		function(node,style){
+			self.nodes.push({text:node,type:style});
+		}
+	);
+}
+
+Nodes.prototype.getNodesQuantity = function(nodetype,nodetext){
+	var quantity = 0;
+	for(var i=0; i<this.nodes.length; i++){
+		if(this.nodes[i].type == nodetype && this.nodes[i].text == nodetext){
+			quantity++;
+		}
+	}
+	return quantity;
+}
+
+
+Nodes.prototype.markCyrillicNodes = function(){
+	for(var i = 0; i < this.nodes.length; i++){
+		if(isCyryllicText(this.nodes[i].text)){
+			this.nodes[i].type = "cyrtext";
+		}
+	}
+}
+
+Nodes.prototype.markSpaceNodes = function(){
+	for(var i = 0; i < this.nodes.length; i++){
+		if(this.nodes[i].text=="\n"){
+			this.nodes[i].type = "linebreak";
+		} else if(/^\s+$/.test(this.nodes[i].text)) {
+			this.nodes[i].type = "space";
+		}
+	}
+}
+
+Nodes.prototype.joinCyrillicNodes = function(){
+	for(var i = 0; i < this.nodes.length; i++){
+		// TODO: объединять не по одной, а по несколько. А то тормозит. Переделать!
+		if(this.nodes[i].type == "cyrtext" && this.nodes[i+1].type == "cyrtext"){
+			this.nodes[i].text += this.nodes[i + 1].text;
+			this.nodes.splice(i + 1, 1);
+			i--;
+		}
+	}
+}
+
+Nodes.prototype.prepareNodes = function(){
+	this.markSpaceNodes();
+	this.markCyrillicNodes();
+	this.joinCyrillicNodes();
+}
+
+// Вспомогательные - вынести!
+
+function isCyryllicText(text){
+	return /^[А-ЯЁ]+$/i.test(text);
+}
--- webui/playground.js
+++ webui/playground.js
@@ -43,79 +43,9 @@ function getNodesAsIs(){
 	return new Nodes(myCodeMirror.getValue());
 };
 
-// конструктор
-function Nodes(text) {
-  this.nodes = [];
-  if(text){
-	  this.fromText(text);
-  }
-}
-
-Nodes.prototype.fromText = function(text) {
-	this.nodes = [];
-	var self = this;
-	CodeMirror.runMode(
-		text,
-		{name:'stex'},
-		function(node,style){
-			self.nodes.push({text:node,type:style});
-		}
-	);
-}
-
-Nodes.prototype.getNodesQuantity = function(nodetype,nodetext){
-	var quantity = 0;
-	for(var i=0; i<this.nodes.length; i++){
-		if(this.nodes[i].type == nodetype && this.nodes[i].text == nodetext){
-			quantity++;
-		}
-	}
-	return quantity;
-}
-
-function isCyryllicText(text){
-	return /^[А-ЯЁ]+$/i.test(text);
-}
-
-
-function markCyrillicNodes(nodes){
-	for(var i = 0; i < nodes.length; i++){
-		if(isCyryllicText(nodes[i].text)){
-			nodes[i].type = "cyrtext";
-		}
-	}
-}
-
-function markSpaceNodes(nodes){
-	for(var i = 0; i < nodes.length; i++){
-		if(nodes[i].text=="\n"){
-			nodes[i].type = "linebreak";
-		} else if(/^\s+$/.test(nodes[i].text)) {
-			nodes[i].type = "space";
-		}
-	}
-}
-
-function joinCyrillicNodes(nodes){
-	for(var i = 0; i < nodes.length; i++){
-		// TODO: объединять не по одной, а по несколько. А то тормозит. Переделать!
-		if(nodes[i].type == "cyrtext" && nodes[i+1].type == "cyrtext"){
-			nodes[i].text += nodes[i + 1].text;
-			nodes.splice(i + 1, 1);
-			i--;
-		}
-	}
-}
-
-function prepareNodes(nodes){
-	markSpaceNodes(nodes);
-	markCyrillicNodes(nodes);
-	joinCyrillicNodes(nodes);
-}
-
 function runcheck(){
 	var nodesObject = getNodesAsIs();
-	prepareNodes(nodesObject.nodes);
+	nodesObject.prepareNodes();
 	console.log(nodesObject.nodes);
 	if(nodesObject.getNodesQuantity("tag","\\newcommand")+nodesObject.getNodesQuantity("tag","\\renewcommand")){
 		alert('Команды переопределять нельзя!');
