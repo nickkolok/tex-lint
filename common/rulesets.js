5acd3801e52a9af209782df5285a8284a8018422
nickkolok <nickkolok@mail.ru>
2016-12-05 02:56:53 +0300

    Сформирован набор правил для матзаметок

--- common/rulesets.js
+++ common/rulesets.js
@@ -2,7 +2,6 @@
 
 var rulesets = {
 	vzmsh2017: {
-		name: "vzmsh2017",
 		title: "ВЗМШ-2017",
 		url: "https://vzmsh.math-vsu.ru/rules/",
 		rules: [
@@ -10,10 +9,23 @@ var rulesets = {
 			["noautonumformulas",1],
 			["noautonumbiblio",1],
 		],
+	},
+	mz: {
+		title: "Математические заметки",
+		url: "http://www.mathnet.ru/php/authornotes.phtml?jrnid=mzm&option_lang=rus&wshow=authornotes",
+		rules: [
+			["Bibitem_exist",1],
+			["udk_exist",1],
+			["udk_onlyone",1],
+		],
 		examples: [
 			{
-				title: "",
-				source: "",
+				title: "С библиографией",
+				source: "mz-01",
+			},
+			{
+				title: "Без библиографии",
+				source: "mz-02-no-biblio",
 			},
 		],
 	},
