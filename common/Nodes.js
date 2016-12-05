772e49e93f68258a8242effec78ae9dd8ef0fa76
nickkolok <nickkolok@mail.ru>
2016-12-06 00:21:02 +0300

    Костыль, худо-бедно обеспечивающий загрузку CodeMirror и в браузере, и в nodejs

--- common/Nodes.js
+++ common/Nodes.js
@@ -1,5 +1,22 @@
 'use strict';
 
+
+// Костыль =(
+// С browserify такого быть не должно
+
+try {
+	if (typeof (navigator) === 'undefined') {
+		var CodeMirror = require("../node_modules/codemirror/addon/runmode/runmode.node.js");
+		require("../node_modules/codemirror/mode/meta.js");
+		require("../node_modules/codemirror/mode/stex/stex.js");
+
+	} else {
+		var CodeMirror = window.CodeMirror;
+	}
+} catch (e) {
+	console.log(e);
+}
+
 // Конструктор
 function Nodes(text) {
   this.nodes = [];
