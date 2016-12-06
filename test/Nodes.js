e29fac1bd381c4d52353c3b53369b740c69a54c7
nickkolok <nickkolok@mail.ru>
2016-12-06 04:03:50 +0300

    Простенькие зачатки тестов на splice

--- test/Nodes.js
+++ test/Nodes.js
@@ -43,8 +43,11 @@ console.log(Nodes6);
 console.log(Nodes6.getNodesNumbers('keyword', '$'));
 
 var Nodes7 = new Nodes('\\frac   \n 1 2');
-Nodes7.nodes[2].text = '\\alpha';
+Nodes7.nodes[2].text = '\\alpha 2 \\beta';
 console.log(Nodes7);
 Nodes7.reparse();
 console.log(Nodes7);
 
+var Nodes8 = new Nodes('\\frac{1+\\alpha}{2 - \\beta}')
+Nodes8.nodes.splice(8,3);
+console.log(Nodes8.toString());
