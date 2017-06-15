484eedfc48b31c53a43495d08a8cd23cb5e0abdc
nickkolok <nickkolok@mail.ru>
2017-06-15 03:54:05 +0300

    Сортировка и уникализация массива индексов в RuleViolation - чтобы в других местах не заморачиваться

--- common/RuleViolation.js
+++ common/RuleViolation.js
@@ -1,10 +1,16 @@
 'use strict';
 
+var unique = require('array-unique');
+
 function RuleViolation(o) {
     for (var prop in o) {
         this[prop] = o[prop];
     }
     if ('indexes' in this) {
+        // Мало ли что сюда передали, чтоб не попортить
+        this.indexes = this.indexes.slice();
+        this.indexes = this.indexes.sort(function(a, b) { return a - b; });
+        unique(this.indexes);
         this.quantity = this.indexes.length;
     }
  }
