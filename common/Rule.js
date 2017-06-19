d38a63152dd4160f06f83f791dac435ca49cd4c2
nickkolok <nickkolok@mail.ru>
2017-06-20 00:50:58 +0300

    Требование заканчивать файл точкой смягчено с учётом окружений.

--- common/Rule.js
+++ common/Rule.js
@@ -597,7 +597,29 @@ new Rule({
 	name: 'dot_ending',
 	message: 'Файл должен заканчиваться точкой или вопросительным знаком',
 	findErrors: function(nodes) {
-		var lastNotNewline = nodes.skipTypesReverse(nodes.length - 1, ['linebreak', 'comment']);
+		// Планируем испортить всю малину
+		nodes = nodes.clone();
+		var lastNotNewline;
+		var ends;
+		do {
+			lastNotNewline = nodes.skipTypesReverse(nodes.length - 1, ['linebreak', 'comment']);
+			if (lastNotNewline < 0) {
+				break;
+			}
+			nodes.length = lastNotNewline + 1;
+
+			ends = nodes.findSingleByRegExp(/^tag$/, /^\\end$/);
+			console.log(ends);
+			if (!ends.length) {
+				break;
+			}
+
+			if (nodes.getArgumentsEnd(ends[ends.length - 1], 2) !== nodes.length) {
+				break;
+			}
+			nodes.replaceArguments(ends[ends.length - 1], 2);
+		} while (1);
+
 		var prelast = nodes.nodes[lastNotNewline];
 		var indexes = [];
 		if (prelast && !(/[\.\?]/).test(prelast.text)) {
