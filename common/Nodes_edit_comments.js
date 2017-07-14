f58b6ccd98f562f3ddade6187bb07c859d3f271e
nickkolok <nickkolok@mail.ru>
2017-07-14 19:05:05 +0300

    Nodes.removeComment() и тесты к ней

--- common/Nodes_edit_comments.js
+++ common/Nodes_edit_comments.js
@@ -35,4 +35,14 @@ Nodes.prototype.removeTrivialComments = function() {
 };
 
 
+Nodes.prototype.removeComment = function(i) {
+	// Прервать комментарий может только конец строки или пробел перед ним
+	var itsLinebreakIndex = this.skipTypes(i + 1, ['space']) + 1;
+	this.nodes.splice(i, itsLinebreakIndex - i - 1);
+	if (this.nodes[i - 1] && this.nodes[i - 1].type === 'linebreak') {
+		this.nodes.splice(i - 1, 1);
+	}
+};
+
+
 };
--- test/Nodes_edit_comments.js
+++ test/Nodes_edit_comments.js
@@ -230,6 +230,154 @@ test('removeTrivialComments', function () {
 });
 
 
+test('removeComment', function () {
+	var N = new Nodes('\\frac{1}{2}3[5]%\n');
+	N.removeComment(11);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]\n',
+		""
+	);
+
+	N = new Nodes('\\frac{1}{2}3[5]% \n');
+	N.removeComment(11);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]\n',
+		""
+	);
+
+	N = new Nodes('\\frac{1}{2}3[5]%\n%');
+	N.removeComment(11);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]\n%',
+		""
+	);
+	N.removeComment(12);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]',
+		""
+	);
+
+	N = new Nodes('\\frac{1}{2}3[5]%\n%');
+	N.removeComment(13);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]%',
+		""
+	);
+
+
+	N = new Nodes('\\frac{1}3[5]%text\n');
+	N.removeComment(8);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}3[5]\n',
+		""
+	);
+
+	N = new Nodes('\\frac{1}{2}3[5]%text\n%line\n');
+	N.removeComment(11);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]\n%line\n',
+		""
+	);
+	N = new Nodes('\\frac{1}{2}3[5]%text\n%line\n');
+	N.removeComment(13);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]%text\n',
+		""
+	);
+
+	N = new Nodes('\\frac{1}{2}3[5]%text\n%line \n');
+	N.removeComment(11);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]\n%line \n',
+		""
+	);
+
+	N = new Nodes('\\frac{1}{2}3[5]%text\n%line \n');
+	N.removeComment(13);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]%text\n',
+		""
+	);
+
+	N = new Nodes('\\frac{1}{2}3[5]%text\n%line \n123');
+	N.removeComment(11);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]\n%line \n123',
+		""
+	);
+	N.removeComment(12);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]\n123',
+		""
+	);
+
+	N = new Nodes('\\frac{1}{2}3[5]%text\n%line \n123');
+	N.removeComment(13);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]%text\n123',
+		""
+	);
+	N.removeComment(11);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]\n123',
+		""
+	);
+
+	N = new Nodes('\\frac{1}{2}3[5]%text\n%line \n123 a b c');
+	N.removeComment(13);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]%text\n123 a b c',
+		""
+	);
+	N.removeComment(11);
+	assert.deepEqual(
+		N.toString(),
+		'\\frac{1}{2}3[5]\n123 a b c',
+		""
+	);
+
+	N = new Nodes('%');
+	N.removeComment(0);
+	assert.deepEqual(
+		N.toString(),
+		'',
+		""
+	);
+
+	N = new Nodes('Текст\ntext % another text');
+	N.removeComment(4);
+	assert.deepEqual(
+		N.toString(),
+		'Текст\ntext ',
+		""
+	);
+
+	N = new Nodes('Текст\ntext % another text\n');
+	N.removeComment(4);
+	assert.deepEqual(
+		N.toString(),
+		'Текст\ntext \n',
+		""
+	);
+
+});
+
+
 /*
 
 test('', function () {
