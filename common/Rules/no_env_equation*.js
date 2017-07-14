cdac15a714b38fe959a830b5d1a892397d94fe83
nickkolok <nickkolok@mail.ru>
2017-07-14 20:03:12 +0300

    Рефакторинг no_env_equation* и его выделение в файл

---
+++ common/Rules/no_env_equation*.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('../Rule.js').forbidEnvs(
+	['equation*'],
+	{
+		name: 'no_env_equation*',
+		message: 'Не разрешается использование окружений \\begin{equation*} ... \\end{equation*}',
+		newBegin: '$$',
+		newEnd: '$$',
+	}
+);
