fc1f82e99f7789d44e67443a45b5d6d6cde6377d
nickkolok <nickkolok@mail.ru>
2017-07-14 22:23:43 +0300

    Индексы в splitrows80

--- common/Rule.js
+++ common/Rule.js
@@ -210,19 +210,23 @@ require('./Rules/no_trivial-comments.js');
 require('./Rules/separate$$.js');
 require('./Rules/separate$.js');
 
-new Rule(
-	"splitrows80",
-	'Длина строки не должна превышать 80 символов',
-	function(nodes) {
-		return {
-			quantity: nodes.getTooLongRowsNumbers(80).length,
-		};
+new Rule({
+	name: 'splitrows80',
+	message: 'Длина строки не должна превышать 80 символов',
+	findErrors: function(nodes) {
+		return new RuleViolation({
+			indexes: nodes.getTooLongRowsIndexes(80),
+		});
 	},
-	function(nodes) {
+	fixErrors: function(nodes) {
 		nodes.splitRows(80);
 		return nodes;
+	},
+	commonCorrector: function(n, i) {
+		n.splitRowByIndex(i, 80);
+		return n;
 	}
-);
+});
 
 require('./Rules/no_env_equation.js');
 require('./Rules/no_env_equation*.js');
