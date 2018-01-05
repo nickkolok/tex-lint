67dbd9006a53c008920282080fd760a58925ae7b
nickkolok <nickkolok@mail.ru>
2018-01-05 23:17:25 +0300

    [parser] `label` etc make their argument `atom` (need to rewrite tests)

--- common/stex-custom.codemirror-mode.js
+++ common/stex-custom.codemirror-mode.js
@@ -71,12 +71,21 @@
     }
 
     var plugins = {};
+    var precise = "variable";
 
     plugins["importmodule"] = addPluginPattern("importmodule", "tag", ["string", "builtin"]);
     plugins["documentclass"] = addPluginPattern("documentclass", "tag", ["", "atom"]);
     plugins["usepackage"] = addPluginPattern("usepackage", "tag", ["atom"]);
-    plugins["begin"] = addPluginPattern("begin", "tag", ["atom"]);
-    plugins["end"] = addPluginPattern("end", "tag", ["atom"]);
+    plugins["begin"] = addPluginPattern("begin", "tag", [precise]);
+    plugins["end"] = addPluginPattern("end", "tag", [precise]);
+
+    plugins["label"    ] = addPluginPattern("label"    , "tag", [precise]);
+    plugins["ref"      ] = addPluginPattern("ref"      , "tag", [precise]);
+    plugins["cite"     ] = addPluginPattern("cite"     , "tag", [precise]);
+    plugins["bibitem"  ] = addPluginPattern("bibitem"  , "tag", [precise]);
+    plugins["Bibitem"  ] = addPluginPattern("Bibitem"  , "tag", [precise]);
+    plugins["eqref"    ] = addPluginPattern("eqref"    , "tag", [precise]);
+    plugins["RBibitem" ] = addPluginPattern("RBibitem" , "tag", [precise]);
 
     plugins["DEFAULT"] = function () {
       this.name = "DEFAULT";
