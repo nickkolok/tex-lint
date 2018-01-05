c6409838bd9768dc44ce9d63015d76140862630c
nickkolok <nickkolok@mail.ru>
2018-01-05 18:10:14 +0300

    Space separation minor improvement

--- common/Nodes_construction.js
+++ common/Nodes_construction.js
@@ -86,18 +86,19 @@ Nodes.prototype.separateSpaces = function() {
 	console.time('Nodes.separateSpaces');
 	// Мы предполагаем, что разрывы строки-то уж кодемирроровский парсер осилил
 	var goodTypesCatalogue = {
-		'cyrtext': null,
+		//'cyrtext': null,
 		'space': null,
 		'linebreak': null,
-		'comment': null,
-		'number': null,
-		'tag': null,
-		'bracket': null,
-		'keyword': null,
+		//'comment': null,
+		//'number': null,
+		//'tag': null,
+		//'bracket': null,
+		//'keyword': null,
+		//'variable': null,
 	};
 
 	for (var i = 0; i < this.length; i++) {
-		if (this.nodes[i].type === 'space' || this.nodes[i].type === 'linebreak') {
+		if (this.nodes[i].type in goodTypesCatalogue) {
 			continue;
 		}
 		if (!(/\s/i).test(this.nodes[i].text)) {
