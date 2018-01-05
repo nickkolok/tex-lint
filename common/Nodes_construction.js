8b17e6deafbd637ae49bdd20daa9f1becd17ae70
nickkolok <nickkolok@mail.ru>
2018-01-05 18:24:22 +0300

    separateNumbers - WIP

--- common/Nodes_construction.js
+++ common/Nodes_construction.js
@@ -55,7 +55,23 @@ Nodes.prototype.separateNumbers = function() {
 	// Но в перспективе - резать по-человечески и обобщить с separateSpaces
 
 	// Мы предполагаем, что разрывы строки-то уж кодемирроровский парсер осилил
+	var goodTypesCatalogue = {
+		'cyrtext': null,
+		'space': null,
+		'linebreak': null,
+		'comment': null,
+		'number': null,
+		'tag': null,
+		'bracket': null,
+		'keyword': null,
+		'variable': null,
+	};
+
 	for (var i = 0; i < this.length; i++) {
+		if (this.nodes[i].type in goodTypesCatalogue) {
+			continue;
+		}
+
 		if (
 			[
 				'space',
