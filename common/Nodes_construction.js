e9f74e695ab3885a91d6ee80b30646cff23f8026
nickkolok <nickkolok@mail.ru>
2018-01-06 05:38:49 +0300

    [parser] Final (I hope) cleanup

--- common/Nodes_construction.js
+++ common/Nodes_construction.js
@@ -78,60 +78,11 @@ Nodes.prototype.fromText = function(text) {
 				self.nodes.push({ text: end[0], type: 'number' });
 				return;
 			}
-
-
-
-
 		}
 	);
 	console.timeEnd('Nodes.fromText');
-	//this.prepareNodes();
 };
 
-Nodes.prototype.markSpaceNodes = function() {
-	console.time('Nodes.markSpaceNodes');
-	var goodTypesCatalogue = {
-		'cyrtext': null,
-		'space': null,
-		'linebreak': null,
-		'comment': null,
-		'number': null,
-		'tag': null,
-		'bracket': null,
-		'keyword': null,
-		'variable': null,
-	};
-	for (var i = 0; i < this.length; i++) {
-		/*
-		if (this.nodes[i].text === '\n') {
-			//console.log(this.nodes[i].type);
-			this.nodes[i].type = 'linebreak';
-		} else
-		*/
-		if (!(this.nodes[i].type in goodTypesCatalogue) && (/^\s+$/i).test(this.nodes[i].text)) {
-			this.nodes[i].type = 'space';
-		}
-	}
-	console.timeEnd('Nodes.markSpaceNodes');
-};
-
-Nodes.prototype.joinNodesOfType = function(type) {
-	console.time('Nodes.joinNodesOfType("' + type + '")');
-	for (var i = 0; i < this.length - 1; i++) {
-		if (this.nodes[i].type === type && this.nodes[i + 1].type === type) {
-			for (var j = i + 1; j < this.length && this.nodes[j].type === type; j++) {
-				this.nodes[i].text += this.nodes[j].text;
-			}
-			this.nodes.splice(i + 1, j - i - 1);
-		}
-	}
-	console.timeEnd('Nodes.joinNodesOfType("' + type + '")');
-};
-
-Nodes.prototype.prepareNodes = function() {
-	this.markSpaceNodes();
-	this.joinNodesOfType('space');
-};
 
 };
 
