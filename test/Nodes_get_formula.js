5ec3e2e17820c7ef7791135d9ef488f45170ff08
NickKolok <nickkolok@mail.ru>
2018-01-24 16:51:01 +0300

    [tests] [WIP] inside* deduplication

--- test/Nodes_get_formula.js
+++ test/Nodes_get_formula.js
@@ -163,6 +163,17 @@ test('isInside$', function () {
 		,
 		[0.5, 1, 0.5, 0, 0.5, 1, 0.5]
 	);
+	testInsideFormulaArray(
+		[
+			'$a$\\alpha$$g$$',
+			'$a$\\alpha\\[g\\]',
+			'\\(a\\)\\alpha$$g$$',
+			'\\(a\\)\\alpha\\[g\\]',
+		],
+		[0, 0, 0, 0, 0.5, 1, 0.5]
+		,
+		[0.5, 1, 0.5, 0, 0, 0, 0]
+	);
 
 	testInsideFormulaArray(
 		[
@@ -181,91 +192,7 @@ test('isInside$', function () {
 	);
 });
 
-test('isInside$', function () {
-	assert.deepEqual(
-		new Nodes('$a$').isInside$(1),
-		true,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$b').isInside$(3),
-		false,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$b').isInside$(2),
-		false,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$b').isInside$(3, true),
-		false,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$b$').isInside$(3, true),
-		false,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$\\alpha$g$').isInside$(5, true),
-		true,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$\\alpha$$g$$').isInside$(5, true),
-		false,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$\\alpha$$g$$123$g$').isInside$(9, true),
-		true,
-		""
-	);
-
-});
-
 test('isInsideFormula', function () {
-	assert.deepEqual(
-		new Nodes('$a$').isInsideFormula(1),
-		true,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$b').isInsideFormula(3),
-		false,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$b').isInsideFormula(2),
-		false,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$b').isInsideFormula(1),
-		true,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$b').isInsideFormula(3, true),
-		false,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$b$').isInsideFormula(3, true),
-		false,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$\\alpha$g$').isInsideFormula(5, true),
-		true,
-		""
-	);
-	assert.deepEqual(
-		new Nodes('$a$\\alpha$$g$$').isInsideFormula(5, true),
-		true,
-		""
-	);
 	assert.deepEqual(
 		new Nodes('$a$\\alpha$$g$$123$g$').isInsideFormula(9, true),
 		true,
