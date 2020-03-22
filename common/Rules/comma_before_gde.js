6fc0da868d1a05e7883296d2b177c1d583711570
NickKolok <nickkolok@mail.ru>
2020-03-22 19:18:11 +0300

    [rules] comma_before_gde: more words

--- common/Rules/comma_before_gde.js
+++ common/Rules/comma_before_gde.js
@@ -6,7 +6,7 @@ var Nodes = require('../Nodes.js').Nodes;
 
 new Rule({
 	name: 'comma_before_gde',
-	message: 'В конце выключной формулы перед словом "где" ставится запятая',
+	message: 'В конце выключной формулы перед словами "где", "который" и т.д. ставится запятая',
 	findErrors: function(nodes) {
 		console.time('Skipping');
 		nodes.setPropByRegExp(/^(space|linebreak|comment)$/, /^/, 'skip', true);
@@ -16,7 +16,7 @@ new Rule({
 		var indexes = nodes.findSequenceByRegExp([
 			{ type: /^/, text: /^(?!(,)$)/ },
 			{ type: /^keyword$/, text: /^\$\$$/ },
-			{ type: /^cyrtext$/, text: /^где$/ },
+			{ type: /^cyrtext$/, text: /^(где$|котор|но$)/ },
 		]);
 		indexes = indexes.map(function(index) {
 			return index + 1;
