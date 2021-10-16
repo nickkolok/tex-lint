5fd118ae257e3ce0ad91ba1625d4f09c94e8375b
NickKolok <nickkolok@mail.ru>
2021-10-17 01:49:39 +0300

    [WIP] isInsideProtectiveTag

--- common/Rules/sin_must_be_command.js
+++ common/Rules/sin_must_be_command.js
@@ -11,9 +11,32 @@ new Rule({
 	message: 'Названия математических операторов, такие как sin, в формулах должны быть прямым шрифтом. Пропущена дробь \\ перед командой',
 	findErrors: function(nodes) {
 		var indexes = nodes.findSingleByRegExp(
-			/variable-2/,
+			/(variable-2)|(null)/,
 			mathopnames.mathOpRegExpInt
 		);
+		indexes = indexes.filter(function(index){
+			return nodes.isInsideFormula(index);
+		})
+		indexes = indexes.filter(function(index){
+			for (var pos = index; pos >= 0; pos--){
+				if(nodes.nodes[pos].text === '}')
+					return true;
+				if(nodes.nodes[pos].type === 'tag')
+					return true;
+				if(nodes.nodes[pos].text === '{')
+					break;
+			}
+			console.log(pos, index);
+			if (pos === -1)
+				return true;
+			for (pos--; pos >= 0; pos--){
+				if(nodes.nodes[pos].text === '}')
+					return true;
+				if(nodes.nodes[pos].type === 'tag')
+					return !nodes.isProtectiveTag(pos);
+			}
+			return true;
+		});
 		return new RuleViolation({
 			indexes: indexes,
 		});
